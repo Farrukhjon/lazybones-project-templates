b2cb047055b4f3d3d9a488c3536262360c8d4bf2
Farrukh Sattorov <firedandy@gmail.com>
2015-11-10 21:52:25 +0500

    Added logger, improved build process and added readme info.

--- templates/java-library/src/main/java/org/farrukh/examples/Main.java
+++ templates/java-library/src/main/java/org/farrukh/examples/Main.java
@@ -1,13 +1,21 @@
 package org.farrukh.examples;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Entry point class for main application.
  */
-@SuppressWarnings({"checkstyle:hideutilityclassconstructor", "checkstyle:javadocmethod"})
+@SuppressWarnings({"checkstyle:hideutilityclassconstructor", "checkstyle:javadocmethod", "checkstyle:constantname"})
 public final class Main {
 
-    public static void main(final String[] args) {
+    /**
+     * Logger backed by logback framework.
+     */
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
+    public static void main(final String[] args) {
+        logger.debug("Running Main class...");
     }
 
 }
