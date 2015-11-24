a9db2963624d7abc21ddac5d13dcc30119e18f0d
Farrukh Sattorov <firedandy@gmail.com>
2015-11-24 10:34:50 +0500

    added base tests and application properties binder.

--- templates/spring-boot-batch-application/src/main/java/org/farrukh/examples/batch/Application.java
+++ templates/spring-boot-batch-application/src/main/java/org/farrukh/examples/batch/Application.java
@@ -1,17 +1,19 @@
-package org.farrukh.examples.spring.batch;
+package org.farrukh.examples.batch;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 /**
  * Spring boot based stand-alone application.
  */
 @SuppressWarnings({"checkstyle:hideutilityclassconstructor", "checkstyle:constantname"})
-@EnableBatchProcessing
+@EnableBatchProcessing // By default it executes all Jobs in the application context on startup.
 @SpringBootApplication
+@EnableConfigurationProperties(ApplicationProperties.class)
 public class Application {
 
     /**
