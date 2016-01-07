3d3e40a4f9134c3e701eaab3d53c47e046f44b1c
Farrukh Sattorov <firedandy@gmail.com>
2016-01-07 11:52:22 +0500

    added some features.

---
+++ templates/spring-boot-application/src/test/groovy/org/farrukh/examples/spring/boot/BaseTest.groovy
@@ -0,0 +1,10 @@
+package org.farrukh.examples.spring.boot
+
+import spock.lang.Specification
+
+/**
+ * Base test class for the tests.
+ */
+@SuppressWarnings('AbstractClassWithoutAbstractMethod')
+abstract class BaseTest extends Specification {
+}
---
+++ templates/spring-boot-application/src/test/groovy/org/farrukh/examples/spring/boot/BaseUnitTest.groovy
@@ -0,0 +1,11 @@
+package org.farrukh.examples.spring.boot
+
+import org.farrukh.examples.spring.boot.category.UnitTestCategory
+import org.junit.experimental.categories.Category
+
+/**
+ * Base unit level class.
+ */
+@Category(UnitTestCategory)
+class BaseUnitTest extends BaseTest {
+}
